714467c8c5235a89b8188b5e383bfa237e7a135d
k8blanco <katiewhite303@gmail.com>
2019-04-06 14:56:10 -0600

    Updates

---
+++ precache-manifest.a43558e2773c2d74880bc8b40537335b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickyclicky/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "94820245913e91fc3f7c",
+    "url": "/clickyclicky/static/js/runtime~main.63c05f55.js"
+  },
+  {
+    "revision": "e338a788a1eda9ae5949",
+    "url": "/clickyclicky/static/js/main.aa3d2f8f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickyclicky/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e338a788a1eda9ae5949",
+    "url": "/clickyclicky/static/css/main.65b6b0cf.chunk.css"
+  },
+  {
+    "revision": "a91d69170f1ef2dc96b214d5646bcb50",
+    "url": "/clickyclicky/index.html"
+  }
+];
