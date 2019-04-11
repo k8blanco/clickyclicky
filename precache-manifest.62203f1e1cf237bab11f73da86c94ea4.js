156d9a754b1169d724a1b25beef2dbd6d391d4c8
k8blanco <katiewhite303@gmail.com>
2019-04-11 16:28:33 -0600

    Updates

--- precache-manifest.62203f1e1cf237bab11f73da86c94ea4.js
+++ precache-manifest.62203f1e1cf237bab11f73da86c94ea4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clickyclicky/static/js/runtime~main.63c05f55.js"
   },
   {
-    "revision": "2ee109a3c72210427893",
-    "url": "/clickyclicky/static/js/main.70796548.chunk.js"
+    "revision": "06c7001cb12b437b8e58",
+    "url": "/clickyclicky/static/js/main.865f1a49.chunk.js"
   },
   {
     "revision": "dd0ed99a380f8d225736",
     "url": "/clickyclicky/static/js/2.884c28c3.chunk.js"
   },
   {
-    "revision": "2ee109a3c72210427893",
-    "url": "/clickyclicky/static/css/main.532f0ce8.chunk.css"
+    "revision": "06c7001cb12b437b8e58",
+    "url": "/clickyclicky/static/css/main.f284aa46.chunk.css"
   },
   {
-    "revision": "28cc8b2b651cfa04393342e58b92f9b5",
+    "revision": "0fade1d8ec833a85a24b0e90ae13773c",
     "url": "/clickyclicky/index.html"
   }
 ];
